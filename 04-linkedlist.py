1bf4b7eebd8d3e767785c53a607b8a519011b0b6
Arbitrary Hubris <chris+arb@neurowerx.com>
2018-09-23 12:32:12 -0500

    updated readme

--- 04-linkedlist.py
+++ 04-linkedlist.py
@@ -7,12 +7,11 @@ from urllib.error import URLError, HTTPError
 def requestURL(req):
     try:
         response = urlopen(req)
+    except HTTPError as e:
+        print('The server couldn\'t fulfill the request.')
+        print('Error code: ', e.code)
     except URLError as e:
-        if hasattr(e, 'reason'):
-            print('We failed to reach a server.')
-            print('Reason: ', e.reason)
-        elif hasattr(e, 'code'):
-            print('The server couldn\'t fulfill the request.')
-            print('Error code: ', e.code)
+        print('We failed to reach a server.')
+        print('Reason: ', e.reason)
     else:
         return response
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Python Challenge Solutions
 
 Solutions from a Python initiate.  Comments, pull requests with interesting/better solutions are welcome.
+
+reminder: on windows venv activate.ps1 is in env/Scripts
+[updated instructions to install posh git on powershell](https://www.develves.net/blogs/asd/articles/using-git-with-powershell-on-windows-10/)
